776c81397f4e85668be3e8f498d0dcd8b8601e6d
Matilde Ritchie <matilderitchie@gmail.com>
2022-05-12 07:18:15 +0200

    it prints to console

--- js/Control/exercise_logic.js
+++ js/Control/exercise_logic.js
@@ -1,7 +1,10 @@
-import * as fileSystem from "fs";
-import Exercise from "../Entity/exercise";
-import Training_Set from "../Entity/training_set";
+//import * as fileSystem from 'fs';
+import Exercise  from '../Entity/exercise.js';
+import Training_Set from '../Entity/training_set.js';
 
+var exercise_set = new Training_Set();  //should be global
+
+/*
 console.log(" Writing into an file ");
 const exercise = new Exercise("What did you have for breakfast?", "required", "forbidden");
 const exercise_set = new Training_Set();
@@ -13,8 +16,9 @@ console.log(" About read the file ")
 const imported = importEx("exercises.json");
 console.log(" Read the file ");
 console.log("question: " + imported);
+*/
 
-function check_solution() {
+function check_solution() {     
 
 }
 
@@ -22,9 +26,49 @@ function next_exercise() {
 
 }
 
-function add_exercise() {
+function get_value(elements){
+  var values = elements.map((o) => o.value);
+   elements.forEach(el => {
+    el.value = ''
+  });
 
+  return values;
 }
+
+export default function add_exercise() {
+  
+  var question = document.getElementById("question").value;
+
+  var symbol = Array.prototype.slice.call(document.getElementsByName('sym[]'));
+  var symbol_val = get_value(symbol);
+
+  var activity = Array.prototype.slice.call(document.getElementsByName('act[]'));
+  var activity_val = get_value(activity);
+
+  var dcr = Array.prototype.slice.call(document.getElementsByName('dcr[]'));
+  var dcr_val = get_value(dcr);
+
+  var req = Array.prototype.slice.call(document.getElementsByName('req[]'));
+  var req_val = get_value(req);
+
+  var hints = Array.prototype.slice.call(document.getElementsByName('hints[]'));
+  var hints_val = get_value(hints);
+
+  const exercise = new Exercise();
+  exercise.question = question;
+  exercise.symbol = symbol_val;
+  exercise.activity = activity_val;
+  exercise.scenario = dcr_val;
+  exercise.req_or_forbidden = req_val;
+  exercise.hints = hints_val;
+
+  exercise_set.add(exercise);
+  
+  const test = JSON.stringify(exercise_set);
+  console.log(test)
+} 
+document.getElementById('add_exercise').addEventListener('click', add_exercise);
+/*
 
 function importEx(path) {
     try{
@@ -36,8 +80,9 @@ function importEx(path) {
     }
 }
 
+
 function exportEx(exercise_set, name = "exercises") {
     let data = JSON.stringify(exercise_set)
     let path = "./" + name + ".json"
     fileSystem.writeFileSync(path, data)
-}
+}*/
--- js/Entity/exercise.js
+++ js/Entity/exercise.js
@@ -1,7 +1,10 @@
 export default class Exercise {
-    constructor (question, required, forbidden) {
+    constructor (question, symbol, activity, scenario, req_or_forbidden, hints) {
         this.question = question;
-        this.required = required;
-        this.forbidden = forbidden;
+        this.symbol = symbol;
+        this.activity = activity;
+        this.scenario = scenario;
+        this.req_or_forbidden = req_or_forbidden;
+        this.hints = hints;
     }
 }
